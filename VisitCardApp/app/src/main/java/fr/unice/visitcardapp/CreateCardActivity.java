f7ab644f76985032b478b65daecbc2c4459de4e3
Dalian Loic <dl100463@etu.unice.fr>
2017-02-14 19:14:01 +0100

    Second commit

---
+++ VisitCardApp/app/src/main/java/fr/unice/visitcardapp/CreateCardActivity.java
@@ -0,0 +1,13 @@
+package fr.unice.visitcardapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CreateCardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_card);
+    }
+}
