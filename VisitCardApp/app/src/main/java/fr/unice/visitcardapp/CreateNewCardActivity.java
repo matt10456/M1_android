c48f80b9323280f0f121eb6093d789db6eb90485
Dalian Loic <dl100463@etu.unice.fr>
2017-03-05 19:35:23 +0100

    Ajout des boites de texte pour créer les cartes de visite

--- VisitCardApp/app/src/main/java/fr/unice/visitcardapp/CreateNewCardActivity.java
+++ VisitCardApp/app/src/main/java/fr/unice/visitcardapp/CreateNewCardActivity.java
@@ -8,6 +8,6 @@ public class CreateNewCardActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_create_from_qr);
+        setContentView(R.layout.activity_create_new_card);
     }
 }
