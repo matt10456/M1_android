dcef41ba127214d3d1b2991e9be1236c04bcd7b9
Dalian Loic <dl100463@etu.unice.fr>
2017-04-09 18:09:06 +0200

    Ajout d'accès aux contacts et récupération du nom de contact sélectionné

--- VisitCardApp/app/src/main/java/fr/unice/visitcardapp/DisplayActivity.java
+++ VisitCardApp/app/src/main/java/fr/unice/visitcardapp/DisplayActivity.java
@@ -1,12 +1,19 @@
 package fr.unice.visitcardapp;
 
+import android.app.Activity;
+import android.content.ContentResolver;
 import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 public class DisplayActivity extends AppCompatActivity {
+    public int PICK_CONTACT_REQUEST = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +28,40 @@ public class DisplayActivity extends AppCompatActivity {
                 startActivity(i);
             }
         });
+
+        Button displayButton2 = (Button)findViewById(R.id.button_display_2);
+        displayButton2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI);
+                startActivityForResult(intent,PICK_CONTACT_REQUEST);
+            }
+        });
+
+    }
+
+    @Override
+    public void onActivityResult( int requestCode, int resultCode, Intent data ) {
+
+        if (resultCode == Activity.RESULT_OK && requestCode == PICK_CONTACT_REQUEST) {
+
+            ContentResolver cr = getContentResolver();
+            Uri dataUri = data.getData();
+            String[] projection = { ContactsContract.Contacts._ID };
+            Cursor cursor = cr.query(dataUri, projection, null, null, null);
+            if ( null != cursor && cursor.moveToFirst()) {
+                String id = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts._ID));
+                String where = ContactsContract.Data.CONTACT_ID + " = ? AND " + ContactsContract.Data.MIMETYPE + " = ?";
+                String[] whereParameters = new String[]{id, ContactsContract.CommonDataKinds.StructuredName.CONTENT_ITEM_TYPE};
+                Cursor nameCur = cr.query(ContactsContract.Data.CONTENT_URI, null, where, whereParameters, null);
+                if (null != nameCur && nameCur.moveToFirst()) {
+                    // Retrieves the name of the selected contact
+                    String formattedName = nameCur.getString(nameCur.getColumnIndex(ContactsContract.CommonDataKinds.StructuredName.DISPLAY_NAME));
+                    //Log.d("TEST",formattedName);
+                }
+            }
+
+        }
     }
 
 }
--- VisitCardApp/app/src/main/java/fr/unice/visitcardapp/MainActivity.java
+++ VisitCardApp/app/src/main/java/fr/unice/visitcardapp/MainActivity.java
@@ -1,12 +1,14 @@
 package fr.unice.visitcardapp;
 
 import android.content.Intent;
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    public int PICK_CONTACT_REQUEST = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +30,15 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent i = new Intent(getApplicationContext(), DisplayActivity.class);
                 startActivity(i);
+            }
+        });
+
+        Button sendButton = (Button)findViewById(R.id.button_send);
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI);
+                startActivityForResult(intent,PICK_CONTACT_REQUEST);
             }
         });
 
