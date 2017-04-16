ff04753c611f8a80eb95419b9910fcd7f68ab240
Dalian Loic <dl100463@etu.unice.fr>
2017-04-16 13:12:59 +0200

    Ajout de géneration de QR code en fonction de texte

--- VisitCardApp/app/src/main/java/fr/unice/visitcardapp/CreateNewCardOrEditActivity.java
+++ VisitCardApp/app/src/main/java/fr/unice/visitcardapp/CreateNewCardOrEditActivity.java
@@ -3,28 +3,60 @@ package fr.unice.visitcardapp;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class CreateNewCardOrEditActivity extends AppCompatActivity {
+    EditText editTextName;
+    EditText editTextSurname;
+    EditText editTextJob;
+    EditText editTextPhone;
+    EditText editTextMail;
+    EditText editTextWebsite;
+    String editTextNameValue;
+    String editTextSurnameValue;
+    String editTextJobValue;
+    String editTextPhoneValue;
+    String editTextMailValue;
+    String editTextWebsiteValue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_new_card);
 
+        editTextName = (EditText)findViewById(R.id.editTextName);
+        editTextSurname = (EditText)findViewById(R.id.editTextSurname);
+        editTextJob = (EditText)findViewById(R.id.editTextJob);
+        editTextPhone = (EditText)findViewById(R.id.editTextPhone);
+        editTextMail = (EditText)findViewById(R.id.editTextEmail);
+        editTextWebsite = (EditText)findViewById(R.id.editTextWebsite);
+
         Button confirmButton = (Button)findViewById(R.id.button_confirm);
         confirmButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String emptyField = "None";
+
+                editTextNameValue = editTextName.getText().length() == 0 ? emptyField : editTextName.getText().toString();
+                editTextSurnameValue = editTextSurname.getText().length() == 0 ? emptyField : editTextSurname.getText().toString();
+                editTextJobValue = editTextJob.getText().length() == 0 ? emptyField : editTextJob.getText().toString();
+                editTextPhoneValue = editTextPhone.getText().length() == 0 ? emptyField : editTextPhone.getText().toString();
+                editTextMailValue = editTextMail.getText().length() == 0 ? emptyField : editTextMail.getText().toString();
+                editTextWebsiteValue = editTextWebsite.getText().length() == 0 ? emptyField : editTextWebsite.getText().toString();
+
+                Log.d("TEST", "1 " + editTextNameValue + " 2 " + editTextSurnameValue + " 3 " + editTextJobValue + " 4 " +
+                        editTextPhoneValue + " 5 " + editTextMailValue + " 6 " + editTextWebsiteValue);
+
                 Intent i = new Intent(getApplicationContext(), CardCreatedActivity.class);
                 startActivity(i);
             }
         });
-
     }
 
     @Override
--- VisitCardApp/app/src/main/java/fr/unice/visitcardapp/DisplayVisitCardActivity.java
+++ VisitCardApp/app/src/main/java/fr/unice/visitcardapp/DisplayVisitCardActivity.java
@@ -1,6 +1,7 @@
 package fr.unice.visitcardapp;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -8,13 +9,25 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.graphics.Bitmap;
+import android.widget.ImageView;
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
 
 public class DisplayVisitCardActivity extends AppCompatActivity {
+    public final static int QRcodeWidth = 500;
+    ImageView imageView;
+    Bitmap bitmap;
+    // The following string should be created according to the previous activity's output
+    String textValue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_card);
+        imageView = (ImageView)findViewById(R.id.imageView);
 
         Button editButton = (Button)findViewById(R.id.button_edit);
         editButton.setOnClickListener(new View.OnClickListener() {
@@ -24,6 +37,15 @@ public class DisplayVisitCardActivity extends AppCompatActivity {
                 startActivity(i);
             }
         });
+
+        textValue = "QRAPP:name=Name,surname=Surname,job=Job,phone=XXXXXXXX,mail=XXXX@XXXX.com,website=XXXX.fr";
+
+        try {
+            bitmap = TextToImageEncode(textValue);
+            imageView.setImageBitmap(bitmap);
+        } catch (WriterException e) {
+            e.printStackTrace();
+        }
 
     }
 
@@ -48,4 +70,36 @@ public class DisplayVisitCardActivity extends AppCompatActivity {
         }
     }
 
+    Bitmap TextToImageEncode(String Value) throws WriterException {
+        BitMatrix bitMatrix;
+        try {
+            bitMatrix = new MultiFormatWriter().encode(
+                    Value,
+                    BarcodeFormat.DATA_MATRIX.QR_CODE,
+                    QRcodeWidth, QRcodeWidth, null
+            );
+
+        } catch (IllegalArgumentException Illegalargumentexception) {
+            return null;
+        }
+        int bitMatrixWidth = bitMatrix.getWidth();
+
+        int bitMatrixHeight = bitMatrix.getHeight();
+
+        int[] pixels = new int[bitMatrixWidth * bitMatrixHeight];
+
+        for (int y = 0; y < bitMatrixHeight; y++) {
+            int offset = y * bitMatrixWidth;
+
+            for (int x = 0; x < bitMatrixWidth; x++) {
+                pixels[offset + x] = bitMatrix.get(x, y) ? Color.BLACK : Color.WHITE;
+            }
+        }
+
+        Bitmap bitmap = Bitmap.createBitmap(bitMatrixWidth, bitMatrixHeight, Bitmap.Config.ARGB_4444);
+
+        bitmap.setPixels(pixels, 0, QRcodeWidth, 0, 0, bitMatrixWidth, bitMatrixHeight);
+        return bitmap;
+    }
 }
+
